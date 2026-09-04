5263ec87d2204d5ab5c0902569a143428f5ca8f8
countdown-br <raaphael.araujo@gmail.com>
2026-09-04 09:35:51 -0300

    feat(data): add two new friends to the list

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
-import dexterPic from "./assets/dexter.jpg"
-import hannibalPic from "./assets/hannibal.jpg"
+import dexterPic from "./assets/dexter.jpg";
+import hannibalPic from "./assets/hannibal.jpg";
+import johanPic from "./assets/johan.jpg";
+import patrickPic from "./assets/jane.jpg";
 
 
 export const friends = [
@@ -8,11 +10,28 @@ export const friends = [
         name: "Dexter",
         lastName: "Morgan",
         avatar: dexterPic,
+        isListed: true,
     },
     {
         id: 2,
         name: "Hannibal",
         lastName: "Lecter",
         avatar: hannibalPic,
+        isListed: true,
+    },
+    {
+        id: 3,
+        name: "Johan",
+        lastName: "Liebert",
+        avatar: johanPic,
+        isListed: true,
+    },
+    {
+        id: 4,
+        name: "Patrick",
+        lastName: "Jane",
+        avatar: patrickPic,
+        isListed: true,
     }
+
 ]
